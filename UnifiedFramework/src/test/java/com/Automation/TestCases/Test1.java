c044fae08ce2f039fc95e13a9af94f423f9889f7
pawan <sharmapawan952@gmail.com>
2017-03-12 19:47:07 +0530

    added project

---
+++ UnifiedFramework/src/test/java/com/Automation/TestCases/Test1.java
@@ -0,0 +1,14 @@
+package com.Automation.TestCases;
+
+import java.net.MalformedURLException;
+
+import com.Automation.UnifiedFramework.ReusableFunctions;
+
+public class Test1 extends ReusableFunctions{
+
+	public Test1(String Element) throws MalformedURLException {
+		super(Element);
+		
+	}
+
+}
---
+++ UnifiedFramework/src/test/java/com/Automation/UnifiedFramework/ReusableFunctions.java
@@ -0,0 +1,40 @@
+package com.Automation.UnifiedFramework;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+
+public class ReusableFunctions extends DriverClass{
+
+	public ReusableFunctions(String Element) throws MalformedURLException {
+		super(Element);
+	}
+
+	
+	protected void sendKeys(By Object,String sendText){
+		try{
+		mydriver.findElement(Object).sendKeys(sendText);
+		mydriver.hideKeyboard();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void clickElement(By Object){
+		try{
+		mydriver.findElement(Object).click();
+		}catch(NoSuchElementException e){
+			e.printStackTrace();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void openNotifications(){
+		
+		mydriver.openNotifications();
+	}
+	
+	
+}
